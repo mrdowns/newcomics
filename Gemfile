7243bb2260dce94313d0644cb6e3937b3bd755f4
matt downs <matt.downs@gmail.com>
2011-10-08 11:14:37 -0400

    replaced prototype with jquery, trying to sort

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.0.10'
 gem 'sqlite3'
 gem 'nokogiri'
 gem 'sass'
+gem 'jquery-rails'
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+jQuery.fn.sort = function(){
+  return this.pushStack(jQuery.makeArray([].sort.apply(this, arguments)));
+};
+$(function(){
+  var list = $("ul.comics");
+  $("*[data-sort-by]").click(function(){
+    var sort_by = $(this).attr("data-sort-by");
+  });
+});
